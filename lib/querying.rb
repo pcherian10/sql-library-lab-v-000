1a2acf279b0d9acf1734f2555445edb9d1ee1f4c
Paul <pcherian10@gmail.com>
2018-06-08 02:25:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,6 +38,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
+
   "SELECT series.title
    FROM series
    INNER JOIN characters
@@ -45,8 +46,6 @@ def select_series_title_with_most_human_characters
    WHERE characters.species = 'human'
    GROUP BY series.title
    ORDER BY COUNT(characters.species) DESC LIMIT 1"
-
-
 
 end
 
