184cdb16bfe8f785bb2fa848fdba9ff9224d32ba
Paul <pcherian10@gmail.com>
2018-06-08 01:47:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,25 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  
+
   "SELECT books.title, books.year
    FROM books
    INNER JOIN series
    ON books.series_id = series.id
    WHERE series.id = 1
    ORDER BY books.year ASC"
-   
+
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  
-  "SELECT characters.name, characters.motto 
+
+  "SELECT characters.name, characters.motto
    FROM characters
    GROUP BY characters.name
    ORDER BY length(characters.motto) DESC LIMIT 1"
-   
+
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  ""
 end
 
 def select_name_and_series_subgenres_of_authors
